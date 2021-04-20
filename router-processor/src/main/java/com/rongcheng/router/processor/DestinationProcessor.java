af102131ef514b733be9381ca878b3822ee89dd2
windy <frcwindy@gmail.com>
2021-04-20 15:39:49 +0800

    生成JSON文件

--- router-processor/src/main/java/com/rongcheng/router/processor/DestinationProcessor.java
+++ router-processor/src/main/java/com/rongcheng/router/processor/DestinationProcessor.java
@@ -1,8 +1,13 @@
 package com.rongcheng.router.processor;
 
 import com.google.auto.service.AutoService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.rongcheng.router.annotations.Destination;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.Writer;
 import java.util.Collections;
 import java.util.Set;
@@ -34,9 +39,9 @@ public class DestinationProcessor extends AbstractProcessor {
 
         System.out.println(TAG + " >>> process start ...");
         //获取注解处理器参数
-        String rootDir =processingEnv.getOptions().get("root_project_dir");
+        String rootDir = processingEnv.getOptions().get("root_project_dir");
 
-        if (rootDir==null){
+        if (rootDir == null) {
             System.out.println(TAG + " Error :>>>  rootDir is null, user root_project_dir to set  ");
             throw new RuntimeException("rootDir = null");
         }
@@ -59,6 +64,7 @@ public class DestinationProcessor extends AbstractProcessor {
         builder.append("    public static Map<String,String> get(){\n\n");
         builder.append("        Map<String,String> mapping= new HashMap<>();\n");
 
+        final JsonArray destinationJsonArray = new JsonArray();
 
         //遍历所有注解信息
         for (Element element : allDestinationElements) {
@@ -81,10 +87,16 @@ public class DestinationProcessor extends AbstractProcessor {
                     .append("\"" + realPath + "\"")
                     .append(");\n");
 
+            JsonObject item = new JsonObject();
+            item.addProperty("url", url);
+            item.addProperty("description", description);
+            item.addProperty("realPath", realPath);
+            destinationJsonArray.add(item);
         }
         builder.append("        return mapping;\n");
         builder.append("    }\n");
         builder.append("}\n");
+
 
         String mappingFullClassName = "com.rong.cheng.router.mapping." + className;
         System.out.println(TAG + " >>> mappingFullClassName = " + mappingFullClassName);
@@ -102,9 +114,35 @@ public class DestinationProcessor extends AbstractProcessor {
         } catch (Exception e) {
             throw new RuntimeException("Error while create file", e);
         }
+        //写入JSON到本地文件中
+        writeJsonFile(rootDir, destinationJsonArray);
+
+
         System.out.println(TAG + " >>> process finish ...");
 
         return false;
+    }
+
+    private void writeJsonFile(String rootDir, JsonArray destinationJsonArray) {
+        File rootDirFile = new File(rootDir);
+        if (!rootDirFile.exists()) {
+            throw new RuntimeException("root_project_dir not exist");
+        }
+        File routerFileDir = new File(rootDirFile, "router_mapping");
+        if (!routerFileDir.exists()) {
+            routerFileDir.mkdir();
+        }
+        File mappingFile = new File(routerFileDir, "mapping_" + System.currentTimeMillis() + ".json");
+        //写入JSON内容
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(mappingFile));
+            String jsonString = destinationJsonArray.toString();
+            out.write(jsonString);
+            out.flush();
+            out.close();
+        } catch (Throwable throwable) {
+            throw new RuntimeException("Error while writing json", throwable);
+        }
     }
 
     /**
